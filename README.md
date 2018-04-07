d2156b6814570bc9ce2ad4ab7b4435fe828466f1
rubenmak <ruben.mak@greenhousegroup.com>
2018-04-07 17:14:00 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # raspberry-pi-ndvi-plant-analysis
-Script for analysis plants using ndvi with Raspberry Pi NoIR camera
+Python code for analysis plants using ndvi with Raspberry Pi NoIR camera
+
+This code is a modification of https://github.com/robintw/RPiNDVI.
+It's modified such that it doesn't show black parts where NDVI is highest in the image.
+Also, the visualisation is modified to my taste and added camera rotation as optional argument.
+
+##Hardware
+Raspberry Pi attached to a screen or VNC (I used 3B and Raspbian with desktop)
+Raspberry Pi NoIR camera with the filter attached in some way
+(more info: https://www.raspberrypi.org/blog/whats-that-blue-thing-doing-here/)
+
+## install dependencies
+Assuming you already have pip and picamera preinstalled. If not:
+```
+sudo apt-get update
+sudo apt-get install python-picamera python-pip
+```
+
+Install opencv using:
+
+```
+sudo pip install opencv-python numpy
+```
+
+## running
+`python audio_analyzer_wav.py /path/to/wav/file`
+
+You might need to set the camera rotation based on how you camera is positioned, e.g.:
+
+`python ndvi.py 90`
+
---
+++ ndvi.py
@@ -0,0 +1,94 @@
+'''
+sudo apt-get update
+sudo apt-get install python-picamera python-pip
+sudo pip install opencv-python numpy
+'''
+
+import sys
+import time
+import numpy as np
+import cv2
+import picamera
+import picamera.array
+
+def label(image, text):
+    """
+    Labels the given image with the given text
+    """
+    return cv2.putText(image, text, (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+
+
+def contrast_stretch(im):
+    """
+    Performs a simple contrast stretch of the given image, from 5-100%.
+    """
+    in_min = np.percentile(im, 5)
+    in_max = np.percentile(im, 100)
+
+    out_min = 0.0
+    out_max = 255.0
+
+    out = im - in_min
+    out *= ((out_min - out_max) / (in_min - in_max))
+    out += in_min
+
+    return out
+
+
+def run():
+    with picamera.PiCamera() as camera:
+        # Set the camera resolution
+        x = 400
+        camera.resolution = (int(1.33 * x), x)
+        # Various optional camera settings below:
+        # camera.framerate = 5
+        # camera.awb_mode = 'off'
+        # camera.awb_gains = (0.5, 0.5)
+        camera.hflip = True
+        camera.vflip = True
+        if len(sys.argv) == 2:
+            camera.rotation = sys.argv[1]
+
+        # Need to sleep to give the camera time to get set up properly
+        time.sleep(1)
+
+        with picamera.array.PiRGBArray(camera) as stream:
+            # Loop constantly
+            while True:
+                # Grab data from the camera, in colour format
+                # NOTE: This comes in BGR rather than RGB, which is important
+                # for later!
+                camera.capture(stream, format='bgr', use_video_port=True)
+                image = stream.array
+
+                # Get the individual colour components of the image
+                b, g, r = cv2.split(image)
+
+                # Calculate the NDVI
+                bottom = (r.astype(float) + b.astype(float))
+                bottom[bottom == 0] = 0.00001  # Make sure we don't divide by zero!
+
+                ndvi = (r.astype(float) - b) / bottom
+                ndvi = contrast_stretch(ndvi)
+                ndvi = ndvi.astype(np.uint8)
+
+                # Do the labelling
+                label(image, 'original')
+                label(ndvi, 'NDVI')
+
+                # Display
+                cv2.imshow('NDVI Image', ndvi)
+                cv2.imshow('Original Image', image)
+
+                stream.truncate(0)
+
+                # If we press ESC then break out of the loop
+                c = cv2.waitKey(7) % 0x100
+                if c == 27:
+                    break
+
+    # Important cleanup here!
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    run()
